e21c7a409b621046495d7b5cdfd1f2d55040a5eb
Nerijus <krekas@gmail.com>
2024-10-16 15:00:27 +0300

    Validation and flash message

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -1,6 +1,9 @@
-import { Link } from '@inertiajs/react';
+import { useState, useEffect } from 'react';
+import { Link, usePage } from '@inertiajs/react';
 
 export default function AppLayout({ children }) {
+    const { flash } = usePage().props;
+
     return (
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
             <nav className="border-b border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800">
@@ -43,6 +46,10 @@ export default function AppLayout({ children }) {
                     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                         <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                             <div className="border-b border-gray-200 bg-white p-6">
+                                {flash.message && (
+                                    <div className="mb-4 rounded-md bg-blue-100 px-3 py-2 text-blue-700">{flash.message}</div>
+                                )}
+
                                 {children}
                             </div>
                         </div>
--- resources/js/Pages/Posts/Create.jsx
+++ resources/js/Pages/Posts/Create.jsx
@@ -22,17 +22,18 @@ export default function CreatePost() {
                     <div>
                         <label htmlFor="title" className="block text-sm font-medium text-gray-700">Title</label>
                         <input onChange={(e) => setData('title', e.target.value)} type="text" id="title" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+                        { errors.title && (<p className="mt-2 text-sm text-red-600">{errors.title}</p>)}
                     </div>
                     <div className="mt-4">
                         <label htmlFor="content" className="block text-sm font-medium text-gray-700">Content</label>
                         <textarea onChange={(e) => setData('content', e.target.value)} type="text" id="content" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
+                        { errors.content && (<p className="mt-2 text-sm text-red-600">{errors.content}</p>)}
                     </div>
                     <div className="mt-4 py-4 space-x-2">
-                        <button type="submit" className="inline-block rounded-md bg-blue-500 px-4 py-3 text-xs font-semibold uppercase tracking-widest text-white shadow-sm">
+                        <button disabled={processing} type="submit" className="inline-block rounded-md bg-blue-500 px-4 py-3 text-xs font-semibold uppercase tracking-widest text-white shadow-sm disabled:opacity-25">
                             Save post
                         </button>
-                        <Link href={route('posts.index')} className="inline-block rounded-md border border-gray-300 px-4 py-3 text-xs font-semibold uppercase tracking-widest shadow-sm"> Cancel
-                        </Link>
+                        <Link href={route('posts.index')} className="inline-block rounded-md border border-gray-300 px-4 py-3 text-xs font-semibold uppercase tracking-widest shadow-sm"> Cancel </Link>
                     </div>
                 </form>
             </div>
